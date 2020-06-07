2733759931126dd38263ac2c3a5978ac955157b1
mrsSGhash <63438240+mrsSGhash@users.noreply.github.com>
2020-06-07 12:47:13 +1200

    Topbit done and config tested
    Added a topbit.php. Check the errors for conenction that occured. Double check username and password for the server used. Database must be created in PHPmyadmin and correspond.

--- config.php
+++ config.php
@@ -1,11 +1,13 @@
 <?php
 
+	
 	ini_set('display_errors', 'On'); //change to 'Off' when live
 	
 	//defining 4 constants
-	define ("DB_NAME","00_12_games");
+	define ("DB_NAME","games_db");
 	define ("DB_USERNAME","root");
-	define ("DB_PASSWORD","");
-	define ("DB_HOST", "localhost");
+	define ("DB_PASSWORD","usbw");
+	define ("DB_HOST", "localhost:3307");
+	
 	
 ?>
