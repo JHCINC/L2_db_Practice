f56c6d13d2f0dd4a2f0c6e8bc69e5a95eff2c35b
mrsSGhash <63438240+mrsSGhash@users.noreply.github.com>
2020-06-07 13:16:50 +1200

    PHP includes for bottombit
    Added php code to index.html and created a bottombit.php file

--- index.php
+++ index.php
@@ -8,18 +8,4 @@
 				</p>
 			</div> <!-- / main -->
 			
-			<div class="box side">
-				<h2>Search Area</h2>
-				
-				<p>
-					This is where the search area goes
-				</p>
-			</div> <!-- / side -->
-			
-			<div class="box footer">
-				<p>Mrs Sarah Greeff 2020</p>
-			</div> <!-- / footer -->
-			
-		</div> <!-- / wrapper -->
-	</body>
-</html>
+<?php include("bottombit.php") ?>
